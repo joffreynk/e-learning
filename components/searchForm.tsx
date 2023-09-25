f4b48a3ec6b6b30619a28df04c49860b6e979669
joffreynk <joyjoffrey@gmail.com>
2023-09-25 19:08:20 +0200

    finalize form

--- components/searchForm.tsx
+++ components/searchForm.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import Image from "next/image"
 import { Input } from "@/components/ui/input"
+import { useState } from "react"
 
 const SearchForm = () => {
+    const [search, setSearch] = useState('');
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
         <label className="flex-center relative w-full max-w-3xl">
@@ -15,6 +19,8 @@ const SearchForm = () => {
             <Input
             className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
             placeholder="Search"
+            onChange={(event: any) =>setSearch(event.target.value)}
+            value={search}
              />
         </label>
 
