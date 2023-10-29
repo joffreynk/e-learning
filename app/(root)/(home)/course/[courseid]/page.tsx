3ebd6303dd6f4db78b4a6ef23f45ea6dd270ac20
joffreynk <joyjoffrey@gmail.com>
2023-10-29 20:51:22 +0200

    add product details

--- app/(root)/(home)/course/[courseid]/page.tsx
+++ app/(root)/(home)/course/[courseid]/page.tsx
@@ -1,4 +1,6 @@
 import { getCourseDetails } from "@/lib/actions";
+import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 200;
 
@@ -7,9 +9,33 @@ export default async function Home({ params }: { params: { courseid: string } })
   const courseDetails = await getCourseDetails(courseid);
 
   return (
-    <main className="flex-center  paddings w-full flex-col">
-      <h2 className=" pt-20 text-white">{courseDetails.title}</h2>
-      <p>{courseDetails.description}</p>
+    <main className="paddings w-full flex flex-col gap-16">
+      <h2 className="text-center underline text-4xl  pb-16 pt-20 text-white">
+        {courseDetails.title}
+      </h2>
+      <div className="flex flex-col gap-8 lg:flex-row">
+        <Image
+          src={courseDetails.image}
+          className="rounded-lg"
+          width={500}
+          height={500}
+          alt="course Image"
+        />
+        <div className="flex flex-col gap-10">
+          <h3>
+            {courseDetails.title} is {courseDetails.category} course for you
+          </h3>
+          <p>{courseDetails.description}</p>
+          <p>Downloads : {courseDetails.views}</p>
+          <Link className="border-l-pink" href={courseDetails.downloadLink}>
+            click to download
+          </Link>
+        </div>
+      </div>
+      <div>
+        <h4>Other Images</h4>
+        
+      </div>
     </main>
   );
 }
